d3e50b73e63028b6eebd196632302a7f32ecfad5
greenmirage <jaconjustin0@hotmail.com>
2019-02-28 09:51:08 -0500

    mise a jour zone

--- demo/zone.py
+++ demo/zone.py
@@ -33,7 +33,36 @@ def canny(big_img):
    # color_img = cv2.drawContours(color_img, contours, 8, (0, 255, 0), -1 )
     cv2.imshow("Contour", color_img)
     cv2.waitKey()
+def pieces(img,coord):
+    x1,y1,w1,h1=coord[0],coord[1],coord[2],coord[3]
+    cv2.line(img, (x1, y1), (x1+w1, y1), (255, 0, 0), 5)
+    cv2.line(img, (x1, y1), (x1 , y1+h1), (255, 0, 0), 5)
+    cv2.line(img, (x1, y1+h1), (x1 + w1, y1+h1), (255, 0, 0), 5)
+    cv2.line(img, (x1+w1, y1+h1), (x1 + w1, y1-h1), (255, 0, 0), 5)
+    big_img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(img, np.array([0, 0, 0]), np.array([180, 255, 110]))
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    color_img = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:12]
+    for c in contours:
+      contour_length = cv2.arcLength(c, True)
+      polygon_points = cv2.approxPolyDP(c, 0.02 * contour_length, True)
+      area = cv2.contourArea(polygon_points)
+      if area >= 16000 and area <= 17000:
+         x, y, w, h = cv2.boundingRect(polygon_points)
+         centerX = (x + w) / 2
+         centerY = (y + h) / 2
+         cv2.rectangle(color_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+         cv2.putText(color_img, 'zone de piece detecte', (x+20 , y + 20), 0, 0.3, (0, 255, 0))
+    #contour_length = cv2.arcLength(contours[2], True)
+    #polygon_points = cv2.approxPolyDP(contours[2], 0.02 * contour_length, True)
+   # area = cv2.contourArea(polygon_points)
+    #lol= len(contours[2])
+   # color_img = cv2.drawContours(color_img, contours, 2, (0, 255, 0), 2)
+    cv2.imshow("Contour2",  color_img)
+    cv2.waitKey()
 
+0
 def DUU(big_img):
     cv2.imshow("img2", big_img)
     mask = cv2.GaussianBlur(big_img.copy(), (7, 7), 0)
@@ -60,10 +89,7 @@ def DUU(big_img):
                x, y, w, h = cv2.boundingRect(polygon_points)
                cv2.rectangle(mask, (x, y), (x + w, y + h), (0, 255, 0), 2)
                cv2.putText(mask, 'zone de depot detecte', (x , y+27), 0, 0.3, (0, 255, 0))
-           elif area >= 14000 and area <= 15200:
-               x, y, w, h = cv2.boundingRect(polygon_points)
-               cv2.rectangle(mask, (x, y), (x + w, y + h), (0, 255, 0), 2)
-               cv2.putText(mask, 'ZONE DE PIECE', (x, y + 27), 0, 0.3, (0, 255, 0))
+
 
 
 
@@ -95,9 +121,10 @@ def detection(big_img):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(3, 3))
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel=kernel)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel=kernel, iterations=3)
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     color_img = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:12]
+    x1,y1,w1,h1=0,0,0,0
     for c in contours:
       contour_length = cv2.arcLength(c, True)
       polygon_points = cv2.approxPolyDP(c, 0.02 * contour_length, True)
@@ -107,20 +134,22 @@ def detection(big_img):
          centerX = (x + w) / 2
          centerY = (y + h) / 2
          cv2.rectangle(color_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-         #cv2.putText(color_img, 'zone de depart detecte', (centerX + 40, centerY + 50), 0, 0.3, (0, 255, 0))
+        # cv2.putText(color_img, 'zone de depart detecte', (centerX + 40, centerY + 50), 0, 0.3, (0, 255, 0))
       elif area >= 40000 and area < 800000:
          x, y, w, h = cv2.boundingRect(polygon_points)
+         x1, y1, w1, h1 =x, y, w, h
          centerX = (x + w) / 2
          centerY = (y + h) / 2
          cv2.rectangle(color_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-         #cv2.putText(color_img, 'table detecte', (centerX + 40, centerY + 50), 0, 0.3, (0, 255, 0))
+         cv2.putText(color_img, 'table detecte', (x+1220, y), 0, 0.3, (0, 255, 0))
       elif area > 4000 and area <= 30000:
           ((x, y), radius) = cv2.minEnclosingCircle(c)
           center = (int(x), int(y))
           cv2.circle(color_img, center, int(radius), (0, 255, 255), 2)
-   # color_img = cv2.drawContours(color_img, contours, 1, (0, 255, 0), 2)
+    #color_img = cv2.drawContours(color_img, contours, 1, (0, 255, 0), 2)
     cv2.imshow("Contour", color_img)
-    cv2.waitKey()
+    #cv2.waitKey()
+    return (x1, y1, w1, h1)
 # contour_length = cv2.arcLength(contours[10], True)
 # polygon_points = cv2.approxPolyDP(contours[10], 0.02 * contour_length, True)
 # x, y, w, h = cv2.boundingRect(polygon_points)
@@ -137,8 +166,10 @@ def detection(big_img):
 # draw contours onto image
 if __name__ == "__main__":
     # execute only if run as a script
-    big_img = cv2.imread("snapshot_1280_720_0.jpg", 1)
+    big_img = cv2.imread("snapshot_1280_720_2.jpg", 1)
     blurred= blur(big_img)
-    detection(big_img)
+    coord = detection(big_img)
+    pieces( cv2.cvtColor(big_img.copy(), cv2.COLOR_BGR2HSV), coord)
     DUU(big_img)
+
 
