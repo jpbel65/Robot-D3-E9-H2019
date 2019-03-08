922ac0533fc3d177b115ea9779a3052d40148d25
frpin33 <frederick.pineault.1@ulaval.ca>
2019-03-08 12:13:12 -0500

    Modif Pololu et Websocket sur le PI

--- Websockets.py
+++ Websockets.py
@@ -1,22 +1,27 @@
 import asyncio
 import websockets
-
+import socket
 
 async def hello(websocket, path):
     name = await websocket.recv()
-    print(f"< {name}")
     if name == "go":
         await websocket.send("ok, je vais ou")
         pos = await websocket.recv()
         print(pos)
+        
+def Connection_Routeur():
+    try:
+        host = socket.gethostbyname('192.168.1.1')
+        s = socket.create_connection((host,80),2)
+        return True
+    except :
+        pass
+    return False
 
-
-async def pong(websocket, path):
-    if path == "ping":
-        await websocket.send("pong")
-
-start_server = websockets.serve(hello, 'localhost', 8765)
-start_server_pong = websockets.serve(pong, 'localhost', 7654)
-
-asyncio.get_event_loop().run_until_complete(start_server_pong)
+connection = False
+while(connection == False) :
+    connection = Connection_Routeur()
+    
+start_server = websockets.serve(hello, "192.168.1.38", 8765)
+asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
--- gif code/pololy.py
+++ gif code/pololy.py
@@ -1,9 +1,16 @@
 import maestro
 servo = maestro.Controller()
-servo.setAccel(0,30)
-servo.setSpeed(0,30)
-servo.setTarget(0,6000)  
-servo.setAccel(1,30)
-servo.setSpeed(1,30)
-servo.setTarget(1,6000) 
+servo.setAccel(4,30)
+servo.setSpeed(4,30) 
+servo.setAccel(5,30)
+servo.setSpeed(5,30)
+find = False
+QRValue = ""
+servo.move(4,6000)
+servo.move(5,6000)
+while(find == False):
+    QRValue, find = maestro.QRCheck()
+
+    
+print(QRValue)
 servo.close()
