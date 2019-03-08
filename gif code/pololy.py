d4de0138a9e8c692cbdff0e5487f6483109b38b8
frpin33 <frederick.pineault.1@ulaval.ca>
2019-03-08 14:11:06 -0500

    Mini modif pour servo prehenseur

--- gif code/pololy.py
+++ gif code/pololy.py
@@ -4,13 +4,18 @@ servo.setAccel(4,30)
 servo.setSpeed(4,30) 
 servo.setAccel(5,30)
 servo.setSpeed(5,30)
-find = False
-QRValue = ""
-servo.move(4,6000)
-servo.move(5,6000)
-while(find == False):
-    QRValue, find = maestro.QRCheck()
-
+servo.setAccel(3,50)
+servo.setSpeed(3,50)
+servo.setRange(3,2000, 10000)
+#find = False
+#QRValue = ""
+#servo.move(4,6000)
+#servo.move(5,6000)
+#while(find == False):
+#    QRValue, find = maestro.QRCheck()
+servo.move(3,2000)
+servo.move(3,10000)
+servo.move(3,6000)
     
-print(QRValue)
+#print(QRValue)
 servo.close()
